753bcf37a5383f79871c9ce5d13ddc08e3d7e8b7
NKennedy31 <ikennedy@frba.utn.edu.ar>
2021-08-28 19:04:08 -0300

    Modificaciones a conversion.c y conversion.h

---
+++ 01-InterfacesyMakefile/Conversion.c
@@ -0,0 +1,13 @@
+#include "Conversion.h"
+
+double Celsius(double fahr){
+    return (5.0/9.0)*(fahr-32);
+}
+
+double Farenheit(double celsius){
+    return ((celsius * (9.0/5.0)) + 32);
+}
+
+
+
+
---
+++ 01-InterfacesyMakefile/Conversion.h
@@ -0,0 +1,8 @@
+#ifndef CONVERSION_H_INCLUDED
+static const int CONVERSION_H_INCLUDED; 
+
+double celsius(double);
+double fahrenheit(double);
+void printTablas(char, float, float, float);
+
+#endif
